e578ba2b0fbaa707ef3d93416f695480592d166d
Maja Francetic <francetic.maja@gmail.com>
2019-10-24 19:21:11 +0200

    Add a Seekbar and ListView that represent a times table between numbers 1 and 10

--- app/src/main/java/com/mfrancetic/timestablesapp/MainActivity.java
+++ app/src/main/java/com/mfrancetic/timestablesapp/MainActivity.java
@@ -3,12 +3,83 @@ package com.mfrancetic.timestablesapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView listView;
+
+    private SeekBar slider;
+
+    private int chosenNumber;
+
+    private List<Integer> numberList;
+
+    private ArrayAdapter arrayAdapter;
+
+    private int minValue = 1;
+
+    private int maxValue = 10;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.list_view);
+        slider = findViewById(R.id.slider);
+
+        numberList = new ArrayList<>();
+
+//        chosenNumber = 2;
+
+        if (chosenNumber < 0) {
+            chosenNumber = 1;
+        }
+
+        updateList(chosenNumber);
+        slider.setProgress(minValue);
+        slider.setMax(maxValue);
+
+        slider.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                if (i < minValue) {
+                    chosenNumber = minValue;
+                } else {
+                    chosenNumber = i;
+                }
+                updateList(chosenNumber);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    private void updateList(int chosenNumber) {
+        numberList.clear();
+        if (chosenNumber < minValue) {
+            chosenNumber = minValue;
+        }
+        for (int i = 1; i <= 10; i++) {
+            numberList.add(chosenNumber * i);
+        }
+        arrayAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_list_item_1,
+                numberList);
+        listView.setAdapter(arrayAdapter);
     }
 }
